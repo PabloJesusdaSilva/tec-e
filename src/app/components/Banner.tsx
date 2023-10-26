6f70bb8c642c2459e62e146611d29ecb135f0676
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-10-25 23:55:35 -0200

    :feat: correcting error in banner image

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,15 +1,31 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 import BannerImage from '@/app/assets/banner.jpg';
 
 const Banner = () => {
    return(
-      <div className="w-full h-[450px]">
+      <div className='w-full h-[450px]'>
          <Image 
             src={BannerImage}
             alt='Image Banner'
-            className='w-full h-full bg-cover'
+            className='w-full h-full object-cover'
          />
+
+         <div className='bg-black/60 w-full h-full relative -top-[100%]' />
+
+         <div className='relative z-20 w-full h-full -top-[190%] ml-36 tracking-wider'>
+            <h2 className='w-2/6 mb-10 text-6xl font-bold leading-tight text-zinc-50'>Conheça os nossos produtos</h2>
+            <p className='font-semibold text-4xl text-zinc-100'>
+               <span className='text-purple-700 mr-2'>
+                  50% off
+               </span> 
+               nos produtos
+            </p>
+
+            <button className='mt-12 py-4 px-8 text-xl rounded-lg tracking-wide font-medium text-zinc-200 bg-purple-700'>
+               Ver produtos
+            </button>
+         </div>
       </div>
    );
 }
